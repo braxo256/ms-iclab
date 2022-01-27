f3f88c05778f3a990ed3aa95d21196611e485cac
braxo256 <ericksonbravo.a@gmail.com>
2022-01-27 19:18:45 -0300

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -4,16 +4,15 @@
 	ejecucion.call()
 */
 def call(name, paso){
-     if (paso == "1"){
+    //  if (paso == "1"){
         // stage("Paso 1:Leer mi Mensaje"){
             echo "Este es mi mensaje: '${name}'"
         // }
-     }
+    //  }
 
-     if(paso =="2"){
-    //      stage("Paso 1:Responder mi Mensaje"){
-             echo "Responder mi mensaje: '${name}'"
-    //     }
-     }    
+    //  if(paso =="2"){
+    // //      stage("Paso 1:Responder mi Mensaje"){
+    //          echo "Responder mi mensaje: '${name}'"
+    // //     }
+    //  }    
 }
-return this;
