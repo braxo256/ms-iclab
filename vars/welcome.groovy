99ffcd3e713bb158c47a1816c88f12dbc01cfb52
Erikson Bravo <48916702+braxo256@users.noreply.github.com>
2022-01-29 17:14:02 -0300

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -17,8 +17,8 @@ def call(name, paso){
      }    
 //	
 //	def repoUrl = env.GIT_URL
-	  echo 'Pulling BRANCH_NAME...' + env.BRANCH_NAME
-	  echo 'Pulling GIT_URL...' + env.GIT_URL
+	echo 'Pulling BRANCH_NAME...' + ${BRANCH_NAME}
+	echo 'Pulling GIT_URL...' + ${GIT_URL}
 	sh "env"
 //def key = repoUrl.split('/')[3]
 //def slug = repoUrl.split('/')[4]
