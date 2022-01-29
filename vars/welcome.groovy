c3f8cc258b6ce5afa2a82f2f86a59c6ef75d9aed
Erikson Bravo <48916702+braxo256@users.noreply.github.com>
2022-01-29 17:49:48 -0300

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -16,29 +16,8 @@ def call(name, paso){
     //     }
      }    
 	
-	nextVersionFromGit()
+	sh "git describe --tags 'git rev-list --tags --max-count=1'"
 	
 
 }
 
-
-def nextVersionFromGit() {
-    def latestVersion = sh(returnStdout: true, script: 'git describe --tags --abbrev=0 --match *.*.* 2> /dev/null || echo 0.0.0').trim()
-    def (major, minor, patch) = latestVersion.tokenize('.').collect { it.toInteger() }
-    def nextVersion
-		
- def scope = 'patch'
-		
-    switch (scope) {
-        case 'major':
-            nextVersion = "${major + 1}.0.0"
-            break
-        case 'minor':
-            nextVersion = "${major}.${minor + 1}.0"
-            break
-        case 'patch':
-            nextVersion = "${major}.${minor}.${patch + 1}"
-            break
-    }
-    echo nextVersion
-}
